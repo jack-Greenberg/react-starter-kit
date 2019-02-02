4a599d5de4d00a006f2f7b12283c19067d5cf9aa
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-02-01 23:22:21 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'none',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    watch: true,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+            },
+
+            {
+                test: /\.js(x*)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
